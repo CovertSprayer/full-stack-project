74bb6994a30d64eb8e0a38263961fb4416e81610
Shreyance <shreyancebodh2013@gmail.com>
2023-01-06 23:42:25 +0530

    manual auth done

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,21 @@
 const User = require('../models/user');
 
 module.exports.profile = function(req, res){
-    return res.render('user_profile', {
-        title: 'User Profile'
-    });
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id, function(err, user){
+            if(user){
+                return res.render('user_profile', {
+                    title: 'User Profile',
+                    user: user
+                });
+            }else{
+                res.redirect('/users/sign-in');
+            }
+        });
+    }else{
+        res.redirect('/users/sign-in');
+    }
+    
 }
 
 module.exports.signUp = function(req, res){
@@ -14,9 +26,26 @@ module.exports.signUp = function(req, res){
 
 // render the sign in page
 module.exports.signIn = function(req, res) {
-    return res.render('user_sign_in',{
-        title: 'Codeial | Sign In'
-    });
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id, function(err, user){
+            if(user){
+                return res.render('user_profile', {
+                    title: 'User Profile',
+                    user: user
+                });
+            }else{
+                return res.redirect('user_sign_in',{
+                    title: 'Codeial | Sign In'
+                });
+            }
+
+        })
+    }else{
+        return res.render('user_sign_in',{
+            title: 'Codeial | Sign In'
+        });
+    }
+    
 }
 
 // get the sign up data
@@ -37,11 +66,35 @@ module.exports.create = function(req, res) {
         } else{
             return res.redirect('back');
         }
-    })
+    });
 }
 
 
 // sign in and create a session for the user
 module.exports.createSession = function(req, res) {
-    //todo
+    
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err) {console.log('error in finding user in signing up'); return;}
+
+        if(user){
+            if(user.password != req.body.password){
+                return res.redirect('back');
+            }
+
+            res.cookie('user_id', user.id);
+            return res.redirect('/users/profile');
+
+        } else{
+            return res.redirect('back');
+        }
+    });
+}
+
+module.exports.deleteSession = function(req, res){
+    if(req.cookies.user_id){
+        res.clearCookie('user_id');
+    }
+
+    return res.redirect('/users/sign-in');
+    
 }
--- routes/users.js
+++ routes/users.js
@@ -12,5 +12,8 @@ router.get('/sign-in', usersController.signIn);
 router.get('/sign-up', usersController.signUp);
 
 router.post('/create', usersController.create);
+router.post('/create-session', usersController.createSession);
+
+router.post('/delete-session', usersController.deleteSession);
 
 module.exports = router;
