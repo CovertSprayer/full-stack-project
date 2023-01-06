88291075c565c11b3a362892449dedff7d527c8e
Shreyance <shreyancebodh2013@gmail.com>
2023-01-06 21:21:49 +0530

    user sign up

--- routes/users.js
+++ routes/users.js
@@ -8,4 +8,9 @@ const postsController = require('../controllers/posts_contoller');
 router.get('/profile', usersController.profile);
 router.get('/posts', postsController.posts);
 
+router.get('/sign-in', usersController.signIn);
+router.get('/sign-up', usersController.signUp);
+
+router.post('/create', usersController.create);
+
 module.exports = router;
